785da7892ca2693c0b649ff3685705b03e968baf
kimia rad <kabbas-rad@myseneca.ca>
2023-07-21 23:07:03 -0400

    init commit

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ app.get('/categories/add',(req, res) => {
 
 app.post('/categories/add', (req, res)=>{
     blogData.addCategory(req.body)
-    .then(category =>{
+    .then(() =>{
         res.redirect("/categories");
     }).catch(err=>{
         res.status(500).send(err);
